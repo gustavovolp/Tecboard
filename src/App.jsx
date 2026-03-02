54281b7b87282ff1f35aa14a6eb783dcb60893bb
Gustavo A. Volpato <gugavolp@hotmail.com>
2026-03-02 20:36:50 -0300

    comit arquivos necessario tecboard

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css'
+
+//  No react, componentes são funções
+// props é um OBJETO, props.children
+
+function TituloFormulario(props){
+  return (
+    <h2>{props.children}</h2>
+  )
+}
+
+function CampoDeFormulario({children}){
+  return(
+    <fieldset>
+      {children}
+    </fieldset>
+  )
+}
+
+function Label({children, htmlFor}){
+  return(
+    <label htmlFor={htmlFor}>
+      {children}
+    </label>
+  )
+}
+
+function CampoDeEntrada({props}){
+  return <input {...props} />
+}
+
+function FormularioDeEvento(){
+  return(
+    <form className='form-evento'>
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
+      <CampoDeFormulario>
+        <Label htmlFor='nome'>Qual o nome do evento?</Label>
+        <CampoDeEntrada type='text' id='nome' placeholder='Summer dev hits' name='nomeEvento'/>
+      </CampoDeFormulario>
+      
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src='/logo.png' alt='logo'/>
+      </header>
+      <section>
+        <img src='/banner.png' alt='Banner Image'/>
+      </section>
+      <FormularioDeEvento/>
+    </main>
+   
+  )
+}
+
+export default App
